7a7aed303aa9bc7902af48dbaaf6974d90cea150
VirtAltoKohi <61061541+MRCGInteract@users.noreply.github.com>
2023-11-17 16:27:48 -0500

    Setting up the project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Group27_Restaurant_Guide"
+include(":app")
+ 
